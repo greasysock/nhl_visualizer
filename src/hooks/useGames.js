cf9d32b3d5fdc0a2dea3dc82b989edec42889997
Chris Gresock <chris.gresock@gmail.com>
2019-09-26 11:55:04 -0400

    Simplified selector

--- src/hooks/useGames.js
+++ src/hooks/useGames.js
@@ -3,10 +3,10 @@ import useTeams from './useTeams'
 
 const useGames = () => {
     const teams = useTeams()
-    const games = useSelector(s=>Object.values(s.games))
-    const filteredGames = games.filter((game)=>teams[game.winnerTeamId] && teams[game.loserTeamId])
-
-    return filteredGames
+    const games = useSelector(s=>Object.values(s.games).filter(game=>{
+        return teams[game.winnerTeamId]&&teams[game.loserTeamId]
+    }))
+    return games
 }
 
 export default useGames
